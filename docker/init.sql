f6be823d32ae274c3ee3917c00534a84863f9854
Adriano Santos <eustaquio.santos.adriano@gmail.com>
2025-04-05 01:18:02 +0200

    trying to fix workflow so it can operate with 2 databases

--- docker/init.sql
+++ docker/init.sql
@@ -3,4 +3,6 @@ CREATE DATABASE IF NOT EXISTS notes;
 
 GRANT ALL PRIVILEGES ON users.* TO 'test_user'@'%';
 GRANT ALL PRIVILEGES ON notes.* TO 'test_user'@'%';
+GRANT CREATE ON *.* TO 'test_user'@'%';  -- Permissão para criar bancos de dados
+
 FLUSH PRIVILEGES;
